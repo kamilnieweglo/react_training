2a14d167d7424aaca8e23ef24a3537739efc65a6
kamilnieweglowski <kamil.nieweglowski@gmail.com>
2020-04-14 10:12:38 +0200

    React header

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>React, Redux and react router for ultra responsive web apps.</p>
+  </div>
+);
+
+export default AboutPage;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./components/App";
+import "./index.css";
+import Header from "./components/common/Header";
 
-function Hi() {
-  return <p>Hi.</p>;
-}
-
-render(<Hi />, document.getElementById("app"));
+render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("app")
+);
